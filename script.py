3da52871b43a3aa908dac1d06820184c75feea7c
andro5 <kestenandrej@gmail.com>
2025-12-16 11:56:47 +0100

    changes

--- script.py
+++ script.py
@@ -1,117 +1,62 @@
 data = [
-    {'vrijeme':'suncano','temp':'visoka','vlaznost':'visoka','vjetar':'slab','y':'ne'},
-    {'vrijeme':'suncano','temp':'visoka','vlaznost':'visoka','vjetar':'jak','y':'ne'},
-    {'vrijeme':'oblacno','temp':'visoka','vlaznost':'visoka','vjetar':'slab','y':'da'},
-    {'vrijeme':'kisno','temp':'srednja','vlaznost':'visoka','vjetar':'slab','y':'da'},
-    {'vrijeme':'kisno','temp':'niska','vlaznost':'normalna','vjetar':'slab','y':'da'},
-    {'vrijeme':'kisno','temp':'niska','vlaznost':'normalna','vjetar':'jak','y':'ne'},
-    {'vrijeme':'oblacno','temp':'niska','vlaznost':'normalna','vjetar':'jak','y':'da'},
-    {'vrijeme':'suncano','temp':'srednja','vlaznost':'visoka','vjetar':'slab','y':'ne'},
-    {'vrijeme':'suncano','temp':'niska','vlaznost':'normalna','vjetar':'slab','y':'da'},
-    {'vrijeme':'kisno','temp':'srednja','vlaznost':'normalna','vjetar':'slab','y':'da'},
-    {'vrijeme':'suncano','temp':'srednja','vlaznost':'normalna','vjetar':'jak','y':'da'},
-    {'vrijeme':'oblacno','temp':'srednja','vlaznost':'visoka','vjetar':'jak','y':'da'},
-    {'vrijeme':'oblacno','temp':'visoka','vlaznost':'normalna','vjetar':'slab','y':'da'},
-    {'vrijeme':'kisno','temp':'srednja','vlaznost':'visoka','vjetar':'jak','y':'ne'}
+    {"vrijeme": "suncano", "temp": "visoka", "vlaznost": "niska", "vjetar": "slab", "odbojka": "da"},
+    {"vrijeme": "suncano", "temp": "visoka", "vlaznost": "visoka", "vjetar": "jak", "odbojka": "ne"},
+    {"vrijeme": "oblacno", "temp": "srednja", "vlaznost": "niska", "vjetar": "slab", "odbojka": "da"},
+    {"vrijeme": "kisovito", "temp": "srednja", "vlaznost": "visoka", "vjetar": "jak", "odbojka": "ne"},
+    {"vrijeme": "oblacno", "temp": "visoka", "vlaznost": "visoka", "vjetar": "slab", "odbojka": "da"},
+    {"vrijeme": "kisovito", "temp": "visoka", "vlaznost": "visoka", "vjetar": "slab", "odbojka": "ne"},
+    {"vrijeme": "suncano", "temp": "niska", "vlaznost": "niska", "vjetar": "jak", "odbojka": "da"},
+    {"vrijeme": "oblacno", "temp": "niska", "vlaznost": "visoka", "vjetar": "slab", "odbojka": "da"},
+    {"vrijeme": "oblacno", "temp": "visoka", "vlaznost": "niska", "vjetar": "slab", "odbojka": "da"},
 ]
 
-# funkcija koja broji koliko puta se vrijednost pojavljuje u datasetu
-def count_where(dataset, atribut, vrijednost, klasa=None):
-    broj = 0
-    for d in dataset:
-        if klasa is None:
-            if d[atribut] == vrijednost:
-                broj += 1
-        else:
-            if d[atribut] == vrijednost and d['y'] == klasa:
-                broj += 1
-    return broj
-
-# treniranje modela naive bayes
-def train(dataset):
-    model = {}
-    total = len(dataset)
-
-    # pronadji klase
-    klase = []
-    for d in dataset:
-        if d['y'] not in klase:
-            klase.append(d['y'])
-
-    # apriorne vjerojatnosti
+# racuna P(y)
+def prior_probabilities(data):
+    total = len(data)
     priors = {}
-    for c in klase:
-        broj_c = 0
-        for d in dataset:
-            if d['y'] == c:
-                broj_c += 1
-        priors[c] = broj_c / total
 
-    # atributi
-    attributes = []
-    for k in dataset[0]:
-        if k != 'y':
-            attributes.append(k)
+    for d in data:
+        y = d["odbojka"]
+        priors[y] = priors.get(y, 0) + 1
 
-    # vrijednosti atributa (skup vrijednosti)
-    values = {}
-    for a in attributes:
-        values[a] = []
-        for d in dataset:
-            if d[a] not in values[a]:
-                values[a].append(d[a])
+    for y in priors:
+        priors[y] /= total
 
-    # uvjetne vjerojatnosti
-    cond_probs = {}
-    for a in attributes:
-        cond_probs[a] = {}
-        for c in klase:
-            cond_probs[a][c] = {}
-            count_c = count_where(dataset, 'y', c)
-            k = len(values[a])
-            for v in values[a]:
-                count_avc = count_where(dataset, a, v, c)
-                cond_probs[a][c][v] = (count_avc + 1) / (count_c + k)
+    return priors
 
-    model['priors'] = priors
-    model['cond_probs'] = cond_probs
-    model['attributes'] = attributes
-    model['values'] = values
-    model['classes'] = klase
-    return model
+# racuna P(x | y)
+def conditional_probability(data, attribute, value, y):
+    subset = [d for d in data if d["odbojka"] == y]
+    total = len(subset)
 
-model = train(data)
+    values = set(d[attribute] for d in data)
+    k = len(values)
 
-# predikcija bez logaritama
-def predict(novi):
-    best_class = None
-    best_prob = -1
+    count = sum(1 for d in subset if d[attribute] == value)
 
-    for c in model['classes']:
-        # pocinjemo od apriorne vjerojatnosti
-        prob = model['priors'][c]
+    return (count + 1) / (total + k)
 
-        # mnozimo uvjetne vjerojatnosti
-        for a in model['attributes']:
-            v = novi[a]
-            if v in model['cond_probs'][a][c]:
-                prob *= model['cond_probs'][a][c][v]
-            else:
-                prob *= 0.000001  # minimalna vrijednost
+# predikcija
+def predict(data, novi):
+    priors = prior_probabilities(data)
+    scores = {}
 
-        # odaberi najvecu probabilnost
-        if prob > best_prob:
-            best_prob = prob
-            best_class = c
+    for y, prior in priors.items():
+        score = prior
 
-    return best_class
+        for attribute, value in novi.items():
+            score *= conditional_probability(data, attribute, value, y)
 
-# primjer podatka
-novi_podatak = {
-    'vrijeme': 'suncano',
-    'temp': 'visoka',
-    'vlaznost': 'normalna',
-    'vjetar': 'slab'
+        scores[y] = score
+
+    return max(scores, key=scores.get)
+
+# test
+novi_dan = {
+    "vrijeme": "suncano",
+    "temp": "visoka",
+    "vlaznost": "visoka",
+    "vjetar": "jak"
 }
 
-print("predikcija:", predict(novi_podatak))
+print("Predikcija:", predict(data, novi_dan))
